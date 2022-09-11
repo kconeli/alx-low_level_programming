96c9e1ff0acf7981a9d4989b9d478258399d57c5
Enock Ondara <ondara1enock@gmail.com>
2022-09-11 14:43:23 -0700

    low programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int c = sizeof(long int);
 int d = sizeof(long long int);
 int e = sizeof(float);
 
-printf("size of a char: %d byte(s)\n", a);
-printf("size of an int: %d byte(s)\n", b);
-printf("size of a long int: %d byte(s)\n", c);
-printf("size of a long long int: %d byte(s)\n", d);
-printf("size of a float: %d byte(s)\n", e);
+printf("Size of a char: %d byte(s)\n", a);
+printf("Size of an int: %d byte(s)\n", b);
+printf("Size of a long int: %d byte(s)\n", c);
+printf("Size of a long long int: %d byte(s)\n", d);
+printf("Size of a float: %d byte(s)\n", e);
 return (0);
 }
