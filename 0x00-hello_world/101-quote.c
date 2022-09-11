beb86afce0bea33a984efceeff2dbefe6798ec2a
Enock Ondara <ondara1enock@gmail.com>
2022-09-11 14:05:53 -0700

    low programming

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+/*
+ * File: 101-quote.c
+ * Auth: Enock Ondara
+ */
+
+#include<unisth.h>
+
+/**
+ * main - Print"and that piece of art is useful" -Dora korpar, 2015-10-19",
+ * 	followed by new line, to standard error.
+ *
+ * Return: Always 1.
+ */
+int main(void)
+{
+	write(2,
+			"and that piece of art is useful\" -Dora Korpar, 2015-10-19\n
+			59);
+
+	return (1);
+}
---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multiligual puzzle");
+	return (0);
+}
