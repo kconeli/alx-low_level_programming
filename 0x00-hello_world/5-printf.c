3ffbf060e0d108d4ec9272dd7646109e592044f5
Enock Ondara <ondara1enock@gmail.com>
2022-09-12 13:11:11 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 
 /**
- * return: always 0 (success)
- */
+* main - Entry point
+* return: always 0 (success)
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
