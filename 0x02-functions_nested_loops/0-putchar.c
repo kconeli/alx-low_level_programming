0671152e95560a93ea145fc815d7d613cef31b05
Enock Ondara <ondara1enock@gmail.com>
2022-09-14 13:58:43 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - main function
+ * Descriptions: print _putchar in new line
+ * Return : 0
+ */
+int main(void)
+{
+	char charName[] = "_putchar";
+	int p;
+
+	for (p = 0; p < 8; p++)
+	{
+		_putchar(charName[p]);
+	}
+	_putchar('\n');
+	return (0);
+}
