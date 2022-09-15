0c2771adfc882e73ffdc3cd345566ac8096435b5
Enock Ondara <ondara1enock@gmail.com>
2022-09-15 14:20:39 -0700

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * islower - check if a character is lower case.
+ *_islower - check if a character is lower case.
  * @c: the character is to be checked
  *Return: 1 if character is lowercase, 0 otherwise
  */
