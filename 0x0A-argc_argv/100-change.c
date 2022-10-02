95e97b2bc5c2291eb1a3bdfdda54b90c2284b1b5
Enock Ondara <ondara1enock@gmail.com>
2022-10-02 09:19:25 -0700

    done all

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,8 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <stdbool.h>
 /**
  * coinConverter - Helper function that does all the mathematics
  * @i: Passed in variable from main for calculations
