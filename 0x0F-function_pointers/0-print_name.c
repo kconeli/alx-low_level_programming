e65d16ad3ab30d2a1d66f4a235dd42a0aa86cf17
Enock Ondara <ondara1enock@gmail.com>
2022-10-13 14:15:18 -0700

    1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,14 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name
- *@name: name of a person
- * @f: function pointer
+ * print_name - Prints a name.
+ * @name: The name to be printed.
+ * @f: A pointer to a function that prints a name.
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-	if ((name) && (f))
-	{
-		(*f)(name);
-	}
-}
-Footer
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
