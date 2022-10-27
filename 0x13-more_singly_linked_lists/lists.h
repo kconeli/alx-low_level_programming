ed1ea49b391ba74eda71c22553cbccf3223345b7
Enock Ondara <ondara1enock@gmail.com>
2022-10-27 05:49:32 -0700

    0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,13 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-/*
- * Desc: Header file containing prototypes and definitions for all functions
- *       and types written in the 0x12-more_singly_linked_lists directory.
- */
-
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -22,6 +17,19 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
+/**
+ * struct listint_s - singly linked list
+ * @p: pointers of nodes 
+ * @next: points to the next node
+ *
+ * Description: singly linked list for pointers
+ */
+typedef struct listp_s
+{
+	int n;
+	struct listp_s *next;
+} listp_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
