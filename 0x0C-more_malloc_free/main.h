ff3109ed1c8e57a8afb78f3e93d8fad50cb5016c
Enock Ondara <ondara1enock@gmail.com>
2022-11-06 07:09:28 -0800

    more malloc done

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,15 +4,14 @@
 /**
  * File: main.h
  * Desc: Header file containing prototypes for all functions
- *       used in the 0x0A-malloc_free directory.
+ *       used in the 0x0B-malloc_free directory.
  */
 
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
-char *argstostr(int ac, char **av);
-char **strtow(char *str);
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif
