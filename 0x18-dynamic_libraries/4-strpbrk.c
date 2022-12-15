22547835a5ff87bab76e9c406315c2c6bbb36bdc
Enock Ondara <ondara1enock@gmail.com>
2022-12-15 15:04:56 -0800

    dynmic all

---
+++ 0x18-dynamic_libraries/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strpbrk - search a string for any of a set bytes
+ * @s: source string
+ * @accept: accepted characters
+ *
+ * Return: the string since the first round of accepted character
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int a;
+
+	while (*s)
+	{
+		for (a = 0; accept[a]; a++)
+		{
+			if (accept[a] == *s)
+				return (s);
+		}
+		s++;
+	}
+
+	return ('\0');
+}
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of integer
+ * @n: the integer to be computed
+ *
+ * Return: absolute value of integer
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
