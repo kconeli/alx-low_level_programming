d1c3d4ef5c25b2c5261f0d733e5497d8cfa0d4e2
Enock Ondara <ondara1enock@gmail.com>
2023-10-07 09:31:46 -0700

    all done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -main fanction
+ *
+ * Return: return 0 every time
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
