9c6e753d68f107d1585211a5449d734ff958651f
Enock Ondara <ondara1enock@gmail.com>
2023-10-12 05:12:04 -0700

    101 sum of div 3&5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - start at main
+ *
+ * prints the sum of all the multiples of 3 or 5
+ * below 1024
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int x;
+	int sum;
+
+	for (x > 0; x < 1024; ++x)
+	{
+		if (x % 3 == 0 || x % 5 == 0)
+		{
+			sum = sum + x;
+		}
+	}
+	printf("%d", sum);
+	printf("\n");
+	return (0);
+}
