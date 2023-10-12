ab79276da44caa2bafd2b0c223dad96831e7f6e3
Enock Ondara <ondara1enock@gmail.com>
2023-10-12 02:36:09 -0700

    to 11

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,11 @@
  */
 void print_alphabet_x10(void)
 {
-	int line,i;
-	for (line=0;line<10; ++line)
+	int line, i;
+
+	for (line = 0; line < 10; ++line)
 	{
-		for (i='a';i<='z';i++)
+		for (i = 'a'; i <= 'z'; i++)
 			_putchar(i);
 		_putchar('\n');
 	}
