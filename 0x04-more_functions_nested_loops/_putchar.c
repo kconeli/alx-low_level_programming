b34c5bbd54b38276de13c96664957fc70492735e
Enock Ondara <ondara1enock@gmail.com>
2023-10-13 03:52:02 -0700

    all

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+r - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: on success 1.
+ * on error, -1 is returned, and errno is appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - calls other funcs
+ *
+ * Returns: always 0
+ */
+
+int main(void)
+{
+	_isupper();
+
+		return (0);
+}
